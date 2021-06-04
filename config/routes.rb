9a86bbe8965f227dbde2e04477230161e3d90f3e
Julia Noiret <julia.noiret@gmail.com>
2021-06-04 12:42:43 +1000

    saved

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'movies/new'
-  get 'bookmarks/new'
-  get 'bookmarks/index'
-  get 'lists/index'
-  get 'lists/new'
-  get 'lists/create'
-  get 'lists/show'
+  # get 'movies/new'
+  # get 'bookmarks/new'
+  # get 'bookmarks/index'
+  # get 'lists/index'
+  # get 'lists/new'
+  # get 'lists/create'
+  # get 'lists/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, only: [ :show, :new, :create] do
     resources :movies, only: [:new, :create]
     resources :bookmarks, only: [:new, :index]
   end
